29d19474093dff1d95beff8f06cf98ddb631e968
박준호 <junho.park@42dot.ai>
2024-03-20 01:15:09 +0900

    version up test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.teacher.english"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
